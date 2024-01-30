856268e20ff326da11c6d9289e237d79d081b1ce
AmyK <amyk@amyks-mbp.staff.campus.up.edu>
2024-01-30 11:18:14 -0800

    rename and clean

--- songparser.js
+++ songparser.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 
 // Read the JSON files
-fs.readFile('top100.json', 'utf8', (err1, data1) => {
-    if (err1) {
-        console.error('Error reading the first file:', err1);
+fs.readFile('top100.json', 'utf8', (err, data1) => {
+    if (err) {
+        console.error('Error reading the file:', err);
         return;
     } try {
         // Parse the JSON data
@@ -42,14 +42,14 @@ fs.readFile('top100.json', 'utf8', (err1, data1) => {
         });
 
         // Restructure the data
-        const restructuredData = data.map(item => ({
-            title: item.attributes.title,
-            released: item.attributes.released,
-            album: albums[item.relationships.album.data.id],
-            artists: item.relationships.artists.data.map(artist => artists[artist.id]),
-            featured_artists: item.relationships.featured_artists.data.map(artist => artists[artist.id]),
-            writers: item.relationships.writers.data.map(writer => writers[writer.id]),
-            genres: item.relationships.genres.data.map(genre => genres[genre.id])
+        const restructuredData = data.map(song => ({
+            title: song.attributes.title,
+            released: song.attributes.released,
+            album: albums[song.relationships.album.data.id],
+            artists: song.relationships.artists.data.map(artist => artists[artist.id]),
+            featured_artists: song.relationships.featured_artists.data.map(artist => artists[artist.id]),
+            writers: song.relationships.writers.data.map(writer => writers[writer.id]),
+            genres: song.relationships.genres.data.map(genre => genres[genre.id])
         }));
 
         // Write the restructured data to a new JSON file
